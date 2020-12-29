3f57f25c30a7bf0f8bc9afd6537111b6d8cea75a
Glitch (hello-webpage) <none>
2020-12-29 19:34:14 +0000

    🎾🌌 Checkpoint ./index.html:25044483/621 ./script.js:23678182/170:25044483/82:25758330/13 ./b_e.js:25044483/61018 ./watch.json:25044483/203

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+var app = new Vue({
+  el: "#app",
+  data: {
+    msg: "hello"
+  },
+  player:{}
+})
