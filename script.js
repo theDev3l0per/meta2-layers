3ad39a59f9b42ec4cd2d40546ccbab045a55e38c
Glitch (hello-webpage) <none>
2020-12-29 19:45:16 +0000

    🏦🌴 Checkpoint ./script.js:23678182/180:25044483/33

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const $ = id => document.getElementById(id)
+const c = id => document.getElementsByClassName(id)
+const t = id => document.getElementsByTagName(id)
+const D = id => new Decimal(id)
+
 var app = new Vue({
   el: "#app",
   data: {
-    msg: "hello"
+    player:{}
   },
-  player:{}
-})
+}) 
