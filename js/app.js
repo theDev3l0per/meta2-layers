64f91e7e905b33686d8564d754439a4f079888df
Glitch (hello-webpage) <none>
2020-12-29 20:32:15 +0000

    🚴🎗 Checkpoint ./js/generators.js:23678182/296:25044483/440 ./index.html:25044483/190 ./style.css:23678182/91 ./style2.css:23678182/145 ./js/app.js:23678182/43

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const D = id => new Decimal(id)
 const app = new Vue({
   el: "#app",
   data: {
-    generators:[{number: 1}, {number: 2}, {number: 3}],
+    generators:[{number: 1}, {number: 2}, {number: 3}, {number: 4}],
     player:{
       pps: "1"
     }
--- js/generators.js
+++ js/generators.js
@@ -1,3 +1,12 @@
+Vue.component("normal-generators", {
+  props: ["generators"],
+  template: `<normal-generator
+        v-for="generator in generators"
+        :number="generator.number"
+        :key="generator.number"
+      ></normal-generator>`
+})
+
 Vue.component("normal-generator", {
   props: ["number", "amt"],
   template: `<button>
